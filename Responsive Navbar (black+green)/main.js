29c2fbf4bad4f1d0e853994aed71ae8bd36948c2
Mahmud Hasan <106822443+mahamudh472@users.noreply.github.com>
2023-03-28 19:54:06 +0600

    Delete Responsive Navbar (black+green) directory

--- Responsive Navbar (black+green)/main.js
+++
@@ -1,9 +0,0 @@
-let hamBurger = document.getElementsByClassName('ham-burger')
-let bars = document.querySelectorAll('.bar')
-hamBurger[0].addEventListener('click', () => {
-    document.getElementsByClassName('nav-right')[0].classList.toggle('active')
-    bars.forEach((element, index) => {
-        element.classList.toggle(`b${index + 1}`)
-    })
-
-})
